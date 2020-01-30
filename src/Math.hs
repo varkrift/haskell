0f543e503e39c2e4566c2cb845e4e13ec6052478
Danil Schuchkin <varkrift@gmail.com>
2020-01-30 23:58:56 +0300

    Factorial in Haskell

--- src/Math.hs
+++ src/Math.hs
@@ -5,3 +5,8 @@ findRoots a b c = if d < 0 then error "No roots" else (x1, x2)
     d = b ^ 2 - 4 * a * c
     x1 = (-b - sqrt d) / (2 * a)
     x2 = (-b + sqrt d) / (2 * a)
+
+factorial5 n | n >= 0    = helper 1 n
+                 | otherwise = error "arg must be >= 0"
+    helper acc 0 = acc
+    helper acc n = helper (acc * n) (n - 1)
