cc5840948447fcfea46908a728f5cafb8082910b
Danil Schuchkin <37618437+varkrift@users.noreply.github.com>
2020-02-11 23:55:31 +0300

    Update Math.hs

--- src/Math.hs
+++ src/Math.hs
@@ -10,3 +10,24 @@ factorial5 n | n >= 0    = helper 1 n
                  | otherwise = error "arg must be >= 0"
     helper acc 0 = acc
     helper acc n = helper (acc * n) (n - 1)
+
+ -- общее определение (все натуральные числа, которые являются простыми :-)
+ primes = [n | n <- [2..], isPrime n]
+ 
+ -- список остатков от деления n на все числа
+ -- из диапазона [2..n/2]
+ listOfRemainders n =[n `mod` x | x <- [2..(n `div` 2)]] 
+ 
+ -- Число простое, если приведённый выше список не содержит нулей
+ isPrime n = null (filter (==0) (listOfRemainders n))
+
+-- С помощью решета Эратосфена:
+
+ ero = eroPrimes [2..]
+ eroPrimes (x : xs) = x : eroPrimes [y | y <- xs , y `mod` x /= 0]
+
+-- И получение, вообще говоря, бесконечного списка простых чисел:
+
+ listOfPrimes = [n | n <- [2..], and [n `mod` (n-x) /= 0 | x <- [1..n-2] ] ]
+
+
